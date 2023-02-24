526356e881331c32a93b807196c8b8e043dad098
Sheshanraj V <117562533+coder-sheshan@users.noreply.github.com>
2023-02-24 12:07:30 +0530

    Add files via upload

--- src/Components/Adidas.js
+++ src/Components/Adidas.js
@@ -2,51 +2,65 @@ import React, { useEffect, useState } from 'react';
 import General from './General';
 import '../Designs/Adidas.css';
 import Button from 'react-bootstrap/esm/Button';
-import { useNavigate } from 'react-router-dom';
 import Footer from './Footer';
 import axios from 'axios';
 
+
 const Adidas = () => {
+
+ 
+  const [resData, setResData] = useState([{ images:[] }]);
+
+  const myFun = (sample) => {
+
+    let Hello = resData.findIndex(({id}) => id === sample)
+
+  // if(Hello){
+  //   setCart(resData.map((id) => id === sample ? 
+  //   {...Hello, quantity : Hello.quantity + 1} : id)
+  //   );
+  // }  else {
+  //   setCart([...resData, {...sample, quantity: 1}]);
+  // }  
+    let Kali = resData[Hello];
+
+    return(
+      // console.log(JSON.stringify(Kali.id, Kali.price))
+      console.log(Kali)
+
+    )
+
+  }
+
   
-const [resData ,setResData] = useState([{image:"."},{image:"1"},{image:"1"}])
-// Make a request for a user with a given ID
-axios.get('https://api.escuelajs.co/api/v1/categories')
+  
+  useEffect(() => {
+
+    axios.get('https://api.escuelajs.co/api/v1/products/?categoryId=4')
   .then(function (response) {
-    // handle success
     setResData(response.data)
-    // console.log(response.data[3].image);
   })
   .catch(function (error) {
-    // handle error
     console.log(error);
   })
-  .then(function () {
-    // always executed
-  });
-  useEffect(() => {
-    console.log(resData[3]?.image)
-  },[resData]);
-
-  const navigate = useNavigate()
+ 
+  }, []);
+   
+    
   return (
     <div id='Adidas'>
-
       <div className='container' data-aos="fade-up">
         <div className='row align-items-md-center  justify-content-around' data-aos="zoom-in" data-aos-delay="100">
-          {resData?.map((dat) => {
-            return(
+          {resData?.map((data) => {
+            return (
               <div className='col-2 col-md-4 align-items-stretch'>
-              <img src={dat?.image} alt='adi shoe1'></img>
-              <h5><span>1,999/-</span> only</h5>
-              <Button onClick={() => { navigate('/addcart') }} variant="outline-primary" size="lg">Add Cart</Button>
-            </div>
+                <img src={data?.images[0]} alt='adi shoe1'></img>
+                <h5><span>{data?.price}</span></h5>
+                <Button onClick={() => {myFun(data.id)}} variant="outline-primary" size="lg">Add Cart</Button>
+              </div>
             )
-
-          })} 
-
+          })}
         </div>
-
-
       </div>
       <General />
       <Footer />
